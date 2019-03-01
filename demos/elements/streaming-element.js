cf3e26a0b5737dad11da7090786ccf8e1ec761ea
Olivier Lange <olange@petit-atelier.ch>
2019-03-01 12:24:05 +0100

    Messages received by ‹osc-stream› limited to a choosen number (#1)

--- demos/elements/streaming-element.js
+++ demos/elements/streaming-element.js
@@ -1,7 +1,6 @@
 import { LitElement, html, css } from "lit-element";
 import { asyncReplace } from "lit-html/directives/async-replace";
 import { repeat } from "lit-html/directives/repeat";
-import { until } from "lit-html/directives/until";
 
 function sleep( delay) {
   return new Promise(( resolve, reject) => setTimeout( resolve, delay));
@@ -26,26 +25,49 @@ export class StreamingElement extends LitElement {
 
   static get properties() {
     return {
-      messages: { type: Array }
+      // Number of messages to keep and display
+      take: { type: Number }
     };
+  }
+
+  // Getter and setter to constrain given attribute value as a
+  // positive integer, defaulting to 1 if value is not numeric
+  get take() { return this._take; }
+  set take( newVal) {
+    const oldVal = this._take;
+    this._take = (newVal && Number.isInteger( newVal) && newVal > 1) ? newVal : 1;
+    if( oldVal !== this._take)
+      this.requestUpdate( "take", oldVal); // Ask Lit-Element to schedule an update
   }
 
   render() {
     return html`
       <slot></slot>
       <p>Time: ${asyncReplace(countUp())}</p>
-      <p>${repeat( this.messages, (message, index) =>
-        html`${index}: ${message}<br/>`)}</p>`;
+      <p>${repeat( this._messages, (message, index) =>
+          html`${this._messageCount - index}: ${message}<br/>`
+      )}</p>${this._messageCount > this._take ?
+          html`<p>(Displaying last ${this._take} messages)</p>` : ""}`;
   }
 
   constructor() {
     super();
     const that = this;
 
-    this.messages = new Array();
+    // Initialize public observed properties
+    this._take = 50;          // Underlying value of `take` property
+
+    // Initialize private property
+    this._messages = [];
+    this._messageCount = 0;
+
+    // Initialize public writable stream
     this.writable = new WritableStream({
       write( message) {
-        that.messages.push( message);
+        that._messageCount += 1;
+        that._messages.unshift( message);
+        if( that._messages.length > that._take)
+          that._messages.splice( that._take);
         that.requestUpdate();
       },
       abort( err) {
